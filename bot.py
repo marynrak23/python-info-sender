db2e1f3ff91e7956964db73b6af517fc47bfd628
Darkside <darkside@iMac-Darkside.local>
2023-08-21 13:53:50 +0300

    add function that getting chat id from message

--- bot.py
+++ bot.py
@@ -3,3 +3,13 @@ import telebot
 
 bot = telebot.TeleBot('6576429362:AAHqaI6T9DnEC4vfKeQmM-lcnfZWK8079ss', parse_mode=None)
 
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    chat_id = message.chat.id
+    print(f"Received a message in chat {chat_id}: {message.text}")
+
+
+bot.infinity_polling()
+
+
